eaeff6344960098e38f1856c40ff4e9c37fd552d
lclar <leah.a.clark1@aexp.com>
2018-06-08 14:24:20 +0100

    finished header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './resources/LOGO.png';
 import './App.css';
+import signup from './resources/signup.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            <img src={logo} alt="twotiltwelve" className="left"/>
+            <img src={signup} alt="twotiltwelve" className="right"/>
+            <h1 className="App-title" >TWO TIL TWELVE</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
