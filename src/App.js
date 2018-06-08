077edb89637880012595fb76dfe09faa8f2148aa
lclar <leah.a.clark1@aexp.com>
2018-06-08 22:44:15 +0100

    welcome page has finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import signup from './resources/signup.png'
 import Header from "./home/Header";
 import Menu from "./home/Menu";
+import Footer from "./home/Footer";
+import Poster from "./resources/Screen Shot 2018-06-08 at 22.19.49.png"
 
 class App extends Component {
   render() {
@@ -13,6 +15,9 @@ class App extends Component {
           <hr />
         <Menu/>
         <hr />
+        <img src={Poster} alt="poster" style={{height: "500px"}}/>
+        <hr />
+        <Footer/>
       </div>
     );
   }
