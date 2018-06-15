262f516cc82a68c0be42e7d490eb82cbeee57695
lclar <leah.a.clark1@aexp.com>
2018-06-15 09:12:05 +0100

    fixed the view on iphone

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
               <HamburgerMenu/> :
               <Menu/>
           }
-          <hr/>
+          <hr className="hr"/>
           <Routing/>
         <Footer/>
       </div>
