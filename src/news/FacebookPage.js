bb0a3fbbb8c31a114123bed1fd63a988144d652a
lclar <leah.a.clark1@aexp.com>
2018-08-24 14:06:03 +0100

    news

--- src/news/FacebookPage.js
+++ src/news/FacebookPage.js
@@ -1,3 +1,28 @@
-/**
- * Created by lclar on 17/08/2018.
- */
+import React, { Component } from 'react';
+import {
+    FBComments,
+    FBCommentsCount,
+    FBEmbedPost,
+    FBEmbedVideo,
+    FBFollow,
+    FBLike,
+    FBPage,
+    FBSend,
+    FBShare
+} from 'facebook-plugins';
+
+class FacebookPage extends Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    <FBPage appId="640433256076330"
+                            href="https://www.facebook.com/twotiltwelve"
+                            tabs={['timeline', 'events', 'messages']}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FacebookPage;
--- src/news/News.js
+++ src/news/News.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Black from "../resources/black_page.jpg"
 import "./news.css"
+import FacebookPage from "./FacebookPage";
+import ErrorBoundary from "../utils/ErrorBoundary";
 
 
 class News extends Component {
     render() {
         return (
             <div className="News">
-                <img className="poster" src={Black} alt="poster" />
+                <ErrorBoundary>
+                <FacebookPage />
+                </ErrorBoundary>
             </div>
         );
     }
