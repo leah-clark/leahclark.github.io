33dae3c9e38843b7a3f368b80da8f4e63f7bbcc5
leah.clark <leah.clark@prodigi.uk>
2019-02-07 00:43:48 +0000

    added facebook

--- src/news/News.js
+++ src/news/News.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Black from "../resources/black_page.jpg"
 import "./news.css"
+import Iframe from './Iframe'
 
 
 class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            src: 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftwotiltwelve%2F&tabs=timeline&width=340&height=490&small_header=true&adapt_container_width=true&hide_cover=true&scrolling=no&show_facepile=false&appId=1635510153337893'
+        };
+    }
+
     render() {
         return (
             <div className="News">
+                <Iframe source={this.state.src} />
             </div>
         );
     }
